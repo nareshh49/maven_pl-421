9f3b9044600ef1f22b24a3e5db81a6dac24c8f86
nareshh49 <124552811+nareshh49@users.noreply.github.com>
2023-08-05 00:16:11 +0530

    first commit

---
+++ src/main/java/com/naresh/App.java
@@ -0,0 +1,10 @@
+package com.naresh;
+
+// import Cowsay class from the library
+
+public class App {
+    public static void main(String[] args) {
+        
+        System.out.printf("Hellowrold");
+    }
+}
